7d82202de0ff9d3d4c1e6728bb243de4bfe214e2
Tatsuya Yatagawa <tatsy.mail@gmail.com>
2021-07-07 03:42:12 +0900

    Minor update.

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension
 
@@ -12,30 +11,32 @@ setup_kwargs = {
     'classifiers': [
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9'
+        'Programming Language :: Python :: 3.9',
     ]
 }
-
 
 try:
     from torch.utils.cpp_extension import CUDAExtension
 
     setup_kwargs.update({
         'ext_modules': [
-              CUDAExtension(
-                  'mcubes_module',
-                  [
-                      'cxx/pscan.cu',
-                      'cxx/mcubes.cpp',
-                      'cxx/mcubes_cpu.cpp',
-                      'cxx/mcubes_cuda.cu',
-                      'cxx/grid_interp_cpu.cpp',
-                      'cxx/grid_interp_cuda.cu',
-                  ],
-                  extra_compile_args=['-DWITH_CUDA'],
-              )
-          ],
-          'cmdclass': {'build_ext': BuildExtension}})
+            CUDAExtension(
+                'mcubes_module',
+                [
+                    'cxx/pscan.cu',
+                    'cxx/mcubes.cpp',
+                    'cxx/mcubes_cpu.cpp',
+                    'cxx/mcubes_cuda.cu',
+                    'cxx/grid_interp_cpu.cpp',
+                    'cxx/grid_interp_cuda.cu',
+                ],
+                extra_compile_args=['-DWITH_CUDA'],
+            )
+        ],
+        'cmdclass': {
+            'build_ext': BuildExtension
+        }
+    })
     setup(**setup_kwargs)
 
 except:
@@ -45,12 +46,15 @@ except:
     from torch.utils.cpp_extension import CppExtension
 
     setup_kwargs.update({
-          'ext_modules': [
-              CppExtension('mcubes_module', [
-                  'cxx/mcubes.cpp',
-                  'cxx/mcubes_cpu.cpp',
-                  'cxx/grid_interp_cpu.cpp',
-              ])
-          ],
-          'cmdclass': {'build_ext': BuildExtension}})
+        'ext_modules': [
+            CppExtension('mcubes_module', [
+                'cxx/mcubes.cpp',
+                'cxx/mcubes_cpu.cpp',
+                'cxx/grid_interp_cpu.cpp',
+            ])
+        ],
+        'cmdclass': {
+            'build_ext': BuildExtension
+        }
+    })
     setup(**setup_kwargs)
